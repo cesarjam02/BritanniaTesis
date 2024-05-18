05581984fb4e7e87eda6b355b2004020454bb6a0
cesarjam02 <108309686+cesarjam02@users.noreply.github.com>
2024-05-17 20:05:43 -0500

    Setup de los archivos iniciales para la App

---
+++ TesisApp/src/test/java/com/TesisBritannia/TesisApp/TesisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TesisBritannia.TesisApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesisAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
